c7ee03441690c3e8838571567da59e263f210b55
JTresoldi <jaaao.a7x@gmail.com>
2026-09-21 19:34:10 -0300

    tarefa2

--- zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Transaction.java
@@ -1,4 +1,16 @@
 package br.com.zenon;
 
-public class Transaction {
+import br.com.zenon.enums.TransactionTypeEnum;
+
+import java.math.BigDecimal;
+
+public record Transaction(
+        int step,
+        TransactionTypeEnum type,
+        BigDecimal amount,
+        TransactionCustomer origin,
+        TransactionCustomer recipient,
+        Boolean isFraud,
+        Boolean isFlaggedFraud
+) {
 }
---
+++ zenon-fraud-detector/src/main/java/br/com/zenon/enums/TransactionTypeEnum.java
@@ -0,0 +1,9 @@
+package br.com.zenon.enums;
+
+public enum TransactionTypeEnum {
+    CASH_IN,
+    CASG_OUT,
+    DEBIT,
+    PAYMENT,
+    TRANSFER
+}
